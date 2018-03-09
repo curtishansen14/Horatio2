83508d990afe7c7e2f219629f84478af96c91472
Curtis Hansen <curtishansen14@gmail.com>
2018-03-09 10:46:33 -0600

    Roles through Migration

---
+++ Horatio 2.0/Controllers/RoleController.cs
@@ -0,0 +1,45 @@
+﻿using Horatio_2._0.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Horatio_2._0.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationRoleManager _roleManager;
+
+        public RoleController()
+        {
+        }
+
+        public RoleController(ApplicationRoleManager roleManager)
+        {
+            RoleManager = roleManager;
+        }
+
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+        // GET: Role
+        public ActionResult Index()
+        {
+            List<RoleViewModel> list = new List<RoleViewModel>();
+            foreach (var role in RoleManager.Roles)
+                list.Add(new RoleViewModel(role));
+            return View(list);
+        }
+
+    }
+}
---
+++ Horatio 2.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Horatio_2._0.MvcApplication" Language="C#" %>
