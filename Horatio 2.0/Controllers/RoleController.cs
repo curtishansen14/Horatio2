34b2be473b5c40134942379aec3916fb390ebd39
Curtis Hansen <curtishansen14@gmail.com>
2018-03-09 10:59:02 -0600

    added Details method to Role Controller

--- Horatio 2.0/Controllers/RoleController.cs
+++ Horatio 2.0/Controllers/RoleController.cs
@@ -69,6 +69,10 @@ namespace Horatio_2._0.Controllers
             return RedirectToAction("Index");
         }
 
-
+        public async Task<ActionResult> Details(string id)
+        {
+            var role = await RoleManager.FindByIdAsync(id);
+            return View(new RoleViewModel(role));
+        }
     }
 }
